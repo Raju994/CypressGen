5faf627757b9f7a59fbec615d7c6a97b1ab947c8
Raju994 <vraju9949@gmail.com>
2024-01-12 12:27:52 +0530

    commit

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require("cypress");
+
+
+
+module.exports = defineConfig({
+  projectId: "fcu6q7",
+  "video":false,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    
+  },
+ 
+ 
+});
